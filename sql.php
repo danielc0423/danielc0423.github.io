74700106e6bddf343f76579eedcae7a2d0c0984d
danielc0423 <49470313+danielc0423@users.noreply.github.com>
2019-04-10 15:38:49 +0800

    Update sql.php

--- sql.php
+++ sql.php
@@ -6,7 +6,7 @@
 // PHP Data Objects(PDO) Sample Code:
     <?php
 try {
-    $conn = new PDO("sqlsrv:server = tcp:hkuspace.database.windows.net,1433; Database = online shopping", "s20081965", "{Swagnok0423}");
+    $conn = new PDO("sqlsrv:server = tcp:hkuspace.database.windows.net,1433; Database = online shopping", "s20081965", "Swagnok0423");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -15,7 +15,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "s20081965@hkuspace", "pwd" => "{Swagnok0423}", "Database" => "online shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "s20081965@hkuspace", "pwd" => "Swagnok0423", "Database" => "online shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:hkuspace.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
