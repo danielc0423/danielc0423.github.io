38f444e76f27ce452048763ab900781ff1b2ff00
danielc0423 <49470313+danielc0423@users.noreply.github.com>
2019-04-14 23:16:53 +0800

    Update sql.php

--- sql.php
+++ sql.php
@@ -2,11 +2,25 @@
 <!DOCTYPE html>
 <html>
 <body>
-
+<?php
+    $username = $POST['username'];
+    $password = $POST['password'];
+    
+    // to prevent sql injection
+    $username =stripcslashes($username);
+    $password =stripcslashes($password);
+    $username =mysql_real_escape_string($username);
+    $password =mysql_real_escape_string($password);
+    
+    
+    
+    
+    
+    
 // PHP Data Objects(PDO) Sample Code:
     <?php
 try {
-    $conn = new PDO("sqlsrv:server = tcp:hkuspace.database.windows.net,1433; Database = online shopping", "s20081965", "Swagnok0423");
+    $conn = new PDO("sqlsrv:server = tcp:hkuspace.database.windows.net,1433; Database = online shopping", "$username", "$password");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -15,7 +29,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "s20081965@hkuspace", "pwd" => "Swagnok0423", "Database" => "online shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "s20081965@hkuspace", "pwd" => "$password", "Database" => "online shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:hkuspace.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
